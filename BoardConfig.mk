81663ccc586f1cfcc91f347a8b1a543b8b5b81b3
Nick Reuter <nreuter85@gmail.com>
2019-06-27 23:11:22 -0500

    dre: convert to Evervolv
    Change-Id: I2f91bc02bc4bb4ab1dc3e96094631464f9b969bd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,8 +93,9 @@ TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/device_framework_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    device/qcom/common/vintf/vendor_framework_compatibility_matrix.xml \
+    hardware/evervolv/interfaces/compatibility_matrices/compatibility_matrix.evervolv.xml \
+    hardware/lineage/interfaces/compatibility_matrices/compatibility_matrix.lineage.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 ODM_MANIFEST_FILES := $(DEVICE_PATH)/manifest_odm.xml
