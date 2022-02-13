d0132ddae0160f90bd0fd60ddd56878011cc0589
Juhyung Park <qkrwngud825@gmail.com>
2022-02-13 18:02:09 +0900

    dre: inherit init.qcom.perf.rc
    dre: move all read_ahead_kb settings to init.aospa.perf.rc
    
    Change-Id: I496d15b2d398d68b59e019e2c917924f829df5c9
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    
    dre: disable zram readahead for all devices
    
    Change-Id: I12a4ffb916c56df30577e75098dec10694c61e66
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -70,6 +70,12 @@ function blob_fixup() {
         vendor/etc/init/vendor.qti.media.c2@1.0-service.rc)
             sed -i "s|writepid /dev/cpuset/foreground/tasks|task_profiles ProcessCapacityHigh|g" "${2}"
              ;;
+        vendor/etc/init/hw/init.qti.kernel.rc)
+            sed -i '/    \# set aggressive read ahead for dm\-0 and dm\-1 during boot up/d' "${2}"
+            sed -i '/    \#Reset read ahead for dm\-0, dm\-1 and dm\-2 to 512kb/d' "${2}"
+            sed -i '/    write \/sys\/block\/dm\-*/d' "${2}"
+            sed -i '/    write \/proc\/sys\/vm\/page-cluster 0/d' "${2}"
+            ;;
         vendor/etc/libnfc-nci.conf)
             sed -i "s/NFC_DEBUG_ENABLED=1/NFC_DEBUG_ENABLED=0/" "${2}"
             ;;
