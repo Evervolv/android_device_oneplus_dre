aedc9834d7adfdd59608431ef89ffc1985248680
Arvind Kumar <quic_karvin@quicinc.com>
2022-03-03 19:22:48 +0530

    holi: Set the model name based on soc id
    Change-Id: Icdc7b4392c40a1f7a53dc7a976bc08d941817683

--- device.mk
+++ device.mk
@@ -258,6 +258,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.qti.early_init.sh \
     init.target.rc \
     ueventd.oplus.rc \
     ueventd.qcom.rc
